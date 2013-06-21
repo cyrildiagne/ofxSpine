25853348081b33a630126ff7057b3a34b3d8357a
Cyril Diagne <diagne.cyril@gmail.com>
2013-06-21 18:22:47 +0200

    added addon_config

---
+++ addon_config.mk
@@ -0,0 +1,6 @@
+meta:
+	ADDON_NAME = ofxSpine
+	ADDON_DESCRIPTION = openFrameworks addon to load and play Spine 2D skeleton animations (using spine-c runtime)
+	ADDON_AUTHOR = Cyril Diagne (@kikko_fr)
+	ADDON_TAGS = "spine" "game" "skeleton" "animation"
+	ADDON_URL = https://github.com/kikko/ofxSpine
